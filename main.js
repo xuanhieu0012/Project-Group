b381b09691bb197852db83ea0327401e00e54092
Hieu <xuanhieu0012@gmail.com>
2021-08-23 11:20:22 -0500

    add pics

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
 // our code goes here
+const animeListURL ='https://api.aniapi.com/v1/anime'
+
+function getAnimeList(){
+    fetch(animeListURL)
+    .then(res => res.json())
+    .then(animeListArray => {console.log(animeListArray.data.documents)
+        displayAnimeList(animeListArray.data.documents)})
+}
+getAnimeList()
+
+function displayAnimeList(animeListArray){
+    const featuredAnime = document.querySelector("#featuredAnime")
+    
+    for(let i = 0; i < 10; i++){
+
+        const createImgTag = document.createElement('img')
+        createImgTag.src = animeListArray[i]["cover_image"]
+        featuredAnime.appendChild(createImgTag)
+    }
+}
